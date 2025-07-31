a61ab828a679743cad1b8a5159214e7b9ee67413
saksham1729 <68738530+saksham1729@users.noreply.github.com>
2025-07-31 18:13:01 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from app import views
 from app.views import ProductView
 from django.conf import settings
 from django.conf.urls.static import static
+
+from app.views import recommend_view
+
 from .views import track_order
 from .views import chatbot_query
 from django.contrib.auth import views as auth_views
@@ -38,6 +41,39 @@ urlpatterns = [
     path('bottomwear/', views.bottomwear, name='bottomwear'),
     path('bottomwear/<slug:data>/', views.bottomwear, name='bottomweardata'),
 
+    path('cables/', views.cables, name='cables'),
+    path('cables/<slug:data>/', views.cables, name='cabledata'),
+
+    path('television/', views.television, name='television'),
+    path('television/<slug:data>/', views.television, name='televisiondata'),
+
+
+    path('office/', views.office, name='office'),
+    path('office/<slug:data>/', views.office, name='officedata'),
+
+    path('kitchen/', views.kitchen, name='kitchen'),
+    path('kitchen/<slug:data>/', views.kitchen, name='kitchendata'),
+
+
+        path('fans/', views.fans, name='fans'),
+    path('fans/<slug:data>/', views.fans, name='fansdata'),
+
+        path('iron/', views.iron, name='iron'),
+    path('iron/<slug:data>/', views.iron, name='irondata'),
+
+        path('grinder/', views.grinder, name='grinder'),
+    path('grinder/<slug:data>/', views.grinder, name='grinderdata'),
+
+        path('roomheater/', views.roomheater, name='roomheater'),
+    path('roomheater/<slug:data>/', views.roomheater, name='roomheaterdata'),
+
+    path('waterheater/', views.waterheater, name='waterheater'),
+    path('waterheater/<slug:data>/', views.waterheater, name='waterheaterdata'),
+
+
+
+
+    path('recommend/<int:user_id>/', recommend_view),
 
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     # path('profile/', views.profile, name='profile'),
@@ -51,11 +87,22 @@ urlpatterns = [
     path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name="password_reset_confirm"),
     path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name="password_reset_complete"),
 
+
     path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
 
     path('payment-success/', views.paymentSuccess, name='payment-success'),
     path('payment-cancel/', views.paymentCancel, name='payment-cancel'),
+
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+
+    path('search/', views.search_products, name='search-products'),
+    path('api/chatbot/', chatbot_query, name='chatbot_query'),
+
+
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+
+    path('rate-product/<int:product_id>/', views.submit_rating, name='submit_rating'),
+
 
     path('search/', views.search_products, name='search-products'),
     path('api/chatbot/', chatbot_query, name='chatbot_query'),
